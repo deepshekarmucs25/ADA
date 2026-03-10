e2756c2a3a8e9391553ad6999ece451e7cd4065a
deepshekarmucs25 <deepshekarmu.cs25@bmsce.ac.in>
2026-03-10 10:02:03 +0530

    Update QuickSort.c

--- QuickSort/QuickSort.c
+++ QuickSort/QuickSort.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
 
+void quick_sort(int a[], int low, int high);
+int partition(int a[], int low, int high);
+void swap(int a[], int i, int j);
+
+void quick_sort(int a[], int low, int high) {
+    if (low < high) {
+        int j = partition(a, low, high);
+        quick_sort(a, low, j - 1);
+        quick_sort(a, j + 1, high);
+    }
+}
+
+int partition(int a[], int low, int high) {
+    int i = low;
+    int j = high + 1;
+    int pivot = a[low];
+
+    while (1) {
+        do {
+            i++;
+        } while (i <= high && a[i] < pivot);
+
+        do {
+            j--;
+        } while (a[j] > pivot);
+
+        if (i >= j)
+            break;
+
+        swap(a, i, j);
+    }
+
+    swap(a, low, j);
+    return j;
+}
+
+void swap(int a[], int i, int j) {
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+
+int main() {
+    int a[10];
+
+    printf("Enter 10 integers:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    quick_sort(a, 0, 9);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
